568e50e5af9e2ea366bd7642f4b89fe906dbbfa7
Paul de Mast <pjf.demast@avans.nl>
2021-02-09 14:24:22 +0100

    Add project files.

---
+++ Lesson2EFMVC/Models/Member.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Lesson2EFMVC.Models {
+    public class Member {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Team Team { get; set; }
+        public int TeamId { get; set; }
+
+    }
+}
---
+++ Lesson2EFMVC/Models/Team.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson2EFMVC.Models {
+    public class Team {
+        public int Id { get; set; }
+        public string Group { get; set; }
+        public string Title { get; set; }
+
+        public ICollection<Member> Enrolled { get; }
+    }
+}
